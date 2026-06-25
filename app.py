e3e4c726bc3890d9400e879be0c3a1b62afd4707
Tmonolit <tmoholit@gmail.com>
2026-06-25 19:01:02 +0500

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '<h1>Второй тест с добавлением текста</h1>'
+    return '<h1>третий тест с добавлением текста</h1>'
     return '<h1>Микросервис успешно развернут в кластере через Jenkins CI/CD!</h1>'
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
