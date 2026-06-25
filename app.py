98754ce3c39d6cdf44ad0df8fc99e5dafa791707
Tmonolit <tmoholit@gmail.com>
2026-06-25 19:08:57 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,34 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '<h1>третий тест с добавлением текста</h1>'
-    return '<h1>Микросервис успешно развернут в кластере через Jenkins CI/CD!</h1>'
+    # Используем тройные кавычки для многострочного текста
+    return '''
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <meta charset="utf-8">
+        <title>Мой DevOps Проект</title>
+    </head>
+    <body style="font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; background-color: #282c34; color: white; text-align: center; padding-top: 100px;">
+        
+        <h1 style="font-size: 50px; color: #61dafb;">
+            Четвертый тест с добавлением текста
+        </h1>
+        
+        <p style="font-size: 24px;">
+            Микросервис успешно развернут в кластере через Jenkins CI/CD!
+        </p>
+        
+        <p style="font-size: 18px; color: #98c379; font-style: italic;">
+            Теперь мы умеем менять дизайн, шрифты и цвета напрямую из кода.
+        </p>
+        
+    </body>
+    </html>
+    '''
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
